f7ac062b73f54adc019c2652f4631a67b6472beb
biegomar <marc.biegota@web.de>
2025-08-01 22:00:42 +0200

    feature: add kata points

---
+++ kata/points.go
@@ -0,0 +1,18 @@
+﻿package kata
+
+import "strings"
+
+func Points(games []string) int {
+	points := 0
+	for _, game := range games {
+		gameResult := strings.Split(game, ":")
+		if gameResult[0] == gameResult[1] {
+			points += 1
+		}
+		if gameResult[0] > gameResult[1] {
+			points += 3
+		}
+	}
+
+	return points
+}
---
+++ kata/points_test.go
@@ -0,0 +1,23 @@
+﻿package kata
+
+import "testing"
+
+func TestPoints(t *testing.T) {
+	testCases := []struct {
+		input    []string
+		expected int
+	}{
+		{[]string{"1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3"}, 30},
+		{[]string{"1:1", "2:2", "3:3", "4:4", "2:2", "3:3", "4:4", "3:3", "4:4", "4:4"}, 10},
+		{[]string{"0:1", "0:2", "0:3", "0:4", "1:2", "1:3", "1:4", "2:3", "2:4", "3:4"}, 0},
+		{[]string{"1:0", "2:0", "3:0", "4:0", "2:1", "1:3", "1:4", "2:3", "2:4", "3:4"}, 15},
+		{[]string{"1:0", "2:0", "3:0", "4:4", "2:2", "3:3", "1:4", "2:3", "2:4", "3:4"}, 12},
+	}
+
+	for _, tc := range testCases {
+		actual := Points(tc.input)
+		if actual != tc.expected {
+			t.Fatalf("Expected %d, got %d", tc.expected, actual)
+		}
+	}
+}
