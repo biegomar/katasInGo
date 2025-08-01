d736b39f5a99133d66761a8376f111e463a96195
biegomar <marc.biegota@web.de>
2025-08-01 22:28:46 +0200

    feature: add kata feast

---
+++ kata/feast_test.go
@@ -0,0 +1,22 @@
+﻿package kata
+
+import "testing"
+
+func TestFeast(t *testing.T) {
+	testCases := []struct {
+		beast    string
+		dish     string
+		expected bool
+	}{
+		{"great blue heron", "garlic naan", true},
+		{"chickadee", "chocolate cake", true},
+		{"brown bear", "bear claw", false},
+	}
+
+	for _, tc := range testCases {
+		actual := Feast(tc.beast, tc.dish)
+		if actual != tc.expected {
+			t.Fatalf("[%s, %s] Expected %t, got %t", tc.beast, tc.dish, tc.expected, actual)
+		}
+	}
+}
