a8c8314f2b04d81f23fd367c1ad729480aab6355
biegomar <marc.biegota@web.de>
2025-08-01 22:51:53 +0200

    feature: add kata catYears

---
+++ kata/catYears.go
@@ -0,0 +1,27 @@
+﻿package kata
+
+func CalculateYears(years int) [3]int {
+	catYears := 0
+	dogYears := 0
+
+	for i := 0; i < years; i++ {
+		switch {
+		case i == 0:
+			{
+				catYears = 15
+				dogYears = 15
+			}
+		case i == 1:
+			{
+				catYears += 9
+				dogYears += 9
+			}
+		default:
+			{
+				catYears += 4
+				dogYears += 5
+			}
+		}
+	}
+	return [3]int{years, catYears, dogYears}
+}
---
+++ kata/catYears_test.go
@@ -0,0 +1,21 @@
+﻿package kata
+
+import "testing"
+
+func TestCatYears(t *testing.T) {
+	testCases := []struct {
+		input    int
+		expected [3]int
+	}{
+		{1, [3]int{1, 15, 15}},
+		{2, [3]int{2, 24, 24}},
+		{10, [3]int{10, 56, 64}},
+	}
+
+	for _, tc := range testCases {
+		actual := CalculateYears(tc.input)
+		if actual != tc.expected {
+			t.Fatalf("Expected %d, got %d", tc.expected, actual)
+		}
+	}
+}
