9d8d8719b1c54fbf62af836d1b65fc51bb18c2a0
biegomar <marc.biegota@web.de>
2025-08-02 22:48:44 +0200

    feature: add kata fixme

---
+++ kata/fixme.go
@@ -0,0 +1,76 @@
+﻿package kata
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Dinglemouse struct {
+	name            string
+	age             int
+	sex             rune
+	orderMap        map[int]string
+	orderMapReverse map[string]int
+	actualPosition  int
+}
+
+func NewDinglemouse() *Dinglemouse {
+	return &Dinglemouse{
+		orderMap:        make(map[int]string),
+		orderMapReverse: make(map[string]int),
+		actualPosition:  0,
+	}
+
+}
+
+func (d *Dinglemouse) SetAge(age int) *Dinglemouse {
+	d.setPosition("age")
+	d.age = age
+	return d
+}
+
+func (d *Dinglemouse) SetSex(sex rune) *Dinglemouse {
+	d.setPosition("sex")
+	d.sex = sex
+	return d
+}
+
+func (d *Dinglemouse) SetName(name string) *Dinglemouse {
+	d.setPosition("name")
+	d.name = name
+	return d
+}
+
+func (d *Dinglemouse) Hello() string {
+	sexText := "male"
+	if d.sex == 'F' {
+		sexText = "female"
+	}
+
+	result := strings.Builder{}
+	result.Grow(100)
+	result.WriteString("Hello.")
+
+	for i := 1; i <= len(d.orderMap); i++ {
+		attribute := d.orderMap[i]
+		if attribute == "name" {
+			result.WriteString(fmt.Sprintf(" My name is %s.", d.name))
+		}
+		if attribute == "sex" {
+			result.WriteString(fmt.Sprintf(" I am %s.", sexText))
+		}
+		if attribute == "age" {
+			result.WriteString(fmt.Sprintf(" I am %d.", d.age))
+		}
+	}
+
+	return result.String()
+}
+
+func (d *Dinglemouse) setPosition(attribute string) {
+	if d.orderMapReverse[attribute] == 0 {
+		d.actualPosition++
+		d.orderMapReverse[attribute] = d.actualPosition
+		d.orderMap[d.actualPosition] = attribute
+	}
+}
---
+++ kata/fixme_test.go
@@ -0,0 +1,59 @@
+﻿package kata
+
+import "testing"
+
+func TestBob27Male(t *testing.T) {
+	//Arrange
+	dm := NewDinglemouse().SetName("Bob").SetAge(27).SetSex('M')
+	expected := "Hello. My name is Bob. I am 27. I am male."
+
+	//Act
+	actual := dm.Hello()
+
+	//Assert
+	if actual != expected {
+		t.Errorf("Expected %s, got %s", expected, actual)
+	}
+}
+
+func Test27MaleBob(t *testing.T) {
+	//Arrange
+	dm := NewDinglemouse().SetAge(27).SetSex('M').SetName("Bob")
+	expected := "Hello. I am 27. I am male. My name is Bob."
+
+	//Act
+	actual := dm.Hello()
+
+	//Assert
+	if actual != expected {
+		t.Errorf("Expected %s, got %s", expected, actual)
+	}
+}
+
+func TestAliceFemale(t *testing.T) {
+	//Arrange
+	dm := NewDinglemouse().SetName("Alice").SetSex('F')
+	expected := "Hello. My name is Alice. I am female."
+
+	//Act
+	actual := dm.Hello()
+
+	//Assert
+	if actual != expected {
+		t.Errorf("Expected %s, got %s", expected, actual)
+	}
+}
+
+func TestBatman(t *testing.T) {
+	//Arrange
+	dm := NewDinglemouse().SetName("Batman")
+	expected := "Hello. My name is Batman."
+
+	//Act
+	actual := dm.Hello()
+
+	//Assert
+	if actual != expected {
+		t.Errorf("Expected %s, got %s", expected, actual)
+	}
+}
