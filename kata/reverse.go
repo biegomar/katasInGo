affa4af3de861826c2481710e6d80509083d81d9
biegomar <marc.biegota@web.de>
2025-08-02 21:29:02 +0200

    feature: add kata reverse

---
+++ kata/reverse.go
@@ -0,0 +1,23 @@
+﻿package kata
+
+import "strings"
+
+func ReverseWords(str string) string {
+	splitted := strings.Split(str, " ")
+
+	for i := 0; i < len(splitted); i++ {
+		splitted[i] = reverseWords(splitted[i])
+	}
+
+	return strings.Join(splitted, " ")
+}
+
+func reverseWords(str string) string {
+	var result = ""
+
+	for i := len(str) - 1; i >= 0; i-- {
+		result += string(str[i])
+	}
+
+	return result
+}
---
+++ kata/reverse_test.go
@@ -0,0 +1,24 @@
+﻿package kata
+
+import "testing"
+
+func TestReverseWords(t *testing.T) {
+	var TestCases = []struct {
+		input    string
+		expected string
+	}{
+		{"The quick brown fox jumps over the lazy dog.", "ehT kciuq nworb xof spmuj revo eht yzal .god"},
+		{"apple", "elppa"},
+		{"a b c d", "a b c d"},
+		{"double  spaced  words", "elbuod  decaps  sdrow"},
+		{"stressed desserts", "desserts stressed"},
+		{"hello hello", "olleh olleh"},
+	}
+
+	for _, tc := range TestCases {
+		actual := ReverseWords(tc.input)
+		if actual != tc.expected {
+			t.Fatalf("Expected %s, got %s", tc.expected, actual)
+		}
+	}
+}
