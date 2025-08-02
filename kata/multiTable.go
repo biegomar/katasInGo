44b0ef8ac5080a7c5727c25f870e97a4c7a6b97b
biegomar <marc.biegota@web.de>
2025-08-02 21:06:56 +0200

    feature: add kata multiplication table

---
+++ kata/multiTable.go
@@ -0,0 +1,18 @@
+﻿package kata
+
+import (
+	"strconv"
+)
+
+func MultiTable(number int) string {
+	var result = ""
+
+	for i := 1; i <= 10; i++ {
+		result = result + strconv.Itoa(i) + " * " + strconv.Itoa(number) + " = " + strconv.Itoa(i*number)
+		if i < 10 {
+			result = result + "\n"
+		}
+	}
+
+	return result
+}
