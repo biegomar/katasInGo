262bdcffd6065cf313589baae7ee199675bc42ed
biegomar <marc.biegota@web.de>
2025-08-02 22:48:31 +0200

    feature: add kata dna

---
+++ kata/dna.go
@@ -0,0 +1,21 @@
+﻿package kata
+
+import "strings"
+
+var dnaComplement = map[rune]string{
+	'A': "T",
+	'T': "A",
+	'C': "G",
+	'G': "C",
+}
+
+func DNAStrand(dna string) string {
+	var result = strings.Builder{}
+	result.Grow(len(dna))
+
+	for _, c := range dna {
+		result.WriteString(dnaComplement[c])
+	}
+
+	return result.String()
+}
---
+++ kata/dna_test.go
@@ -0,0 +1,20 @@
+﻿package kata
+
+import "testing"
+
+func TestDNAStrand(t *testing.T) {
+	var testCases = []struct {
+		input    string
+		expected string
+	}{
+		{"ATTGC", "TAACG"},
+		{"AAAA", "TTTT"},
+		{"GTAT", "CATA"},
+	}
+
+	for _, tc := range testCases {
+		if DNAStrand(tc.input) != tc.expected {
+			t.Fatalf("Expected %s, got %s", tc.expected, DNAStrand(tc.input))
+		}
+	}
+}
