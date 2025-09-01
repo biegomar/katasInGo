c3a9eb328012a2ee44c6930188146dcf4cc04e26
biegomar <marc.biegota@web.de>
2025-09-01 21:58:52 +0200

    feature: add arithmetic mean

---
+++ arithmetic_mean/mean.go
@@ -0,0 +1,16 @@
+﻿package arithmetic_mean
+
+func Mean(numbers []int) float64 {
+	mean := float64(sumForMean(numbers)) / float64(len(numbers))
+
+	return mean
+}
+
+func sumForMean(numbers []int) int {
+	var sum = 0
+	for num := 0; num < len(numbers); num++ {
+		sum += numbers[num]
+	}
+
+	return sum
+}
---
+++ arithmetic_mean/mean_test.go
@@ -0,0 +1,23 @@
+﻿package arithmetic_mean
+
+import "testing"
+
+func TestMean(t *testing.T) {
+	testCases := []struct {
+		numbers  []int
+		expected float64
+	}{
+		{[]int{1, 1, 1}, 1.0},
+		{[]int{1, 2, 3}, 2.0},
+		{[]int{-3, 0, 3, 6}, 1.5},
+		{[]int{2, 4, 6, 8}, 5.0},
+		{[]int{1, 2, 3, 4, 6}, 3.2},
+	}
+
+	for _, tc := range testCases {
+		actual := Mean(tc.numbers)
+		if actual != tc.expected {
+			t.Fatalf("Expected %.2f, got %.2f", tc.expected, actual)
+		}
+	}
+}
