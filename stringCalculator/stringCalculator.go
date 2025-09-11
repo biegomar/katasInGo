e38030f18821ae99631fa5c9f668004c1e50d12c
biegomar <marc.biegota@web.de>
2025-09-11 10:07:21 +0200

    feature: add string calculator

---
+++ stringCalculator/stringCalculator.go
@@ -0,0 +1,23 @@
+﻿package stringCalculator
+
+import (
+	"strconv"
+	"strings"
+)
+
+func Add(numbers string) int {
+	if numbers == "" {
+		return 0
+	}
+
+	values := strings.Split(numbers, ",")
+	result := 0
+	for i := 0; i < len(values); i++ {
+		n, err := strconv.Atoi(values[i])
+		if err == nil {
+			result += n
+		}
+	}
+
+	return result
+}
---
+++ stringCalculator/stringCalculator_test.go
@@ -0,0 +1,23 @@
+﻿package stringCalculator
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	testCases := []struct {
+		stringItems string
+		expected    int
+	}{
+		{"", 0},
+		{"1,2", 3},
+		{"4", 4},
+		{"1,2,3", 6},
+		{"127,23", 150},
+	}
+
+	for _, tc := range testCases {
+		actual := Add(tc.stringItems)
+		if actual != tc.expected {
+			t.Fatalf("[%s] Expected %d, got %d", tc.stringItems, tc.expected, actual)
+		}
+	}
+}
