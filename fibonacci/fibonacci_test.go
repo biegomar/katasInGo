d9a1be5cb5a9b2f44007bc538e50b0b56ea05760
biegomar <marc.biegota@web.de>
2025-09-11 09:54:32 +0200

    feature: add fibonacci

---
+++ fibonacci/fibonacci_test.go
@@ -0,0 +1,28 @@
+﻿package fibonacci
+
+import "testing"
+
+func TestFibonacci(t *testing.T) {
+	testCases := []struct {
+		sequenceNumber int
+		expected       int
+	}{
+		{12, 144},
+		{1, 1},
+		{2, 1},
+		{3, 2},
+		{4, 3},
+		{5, 5},
+		{6, 8},
+		{7, 13},
+		{8, 21},
+		{9, 34},
+	}
+
+	for _, tc := range testCases {
+		actual := Fibonacci(tc.sequenceNumber)
+		if actual != tc.expected {
+			t.Fatalf("[%d] Expected %d, got %d", tc.sequenceNumber, tc.expected, actual)
+		}
+	}
+}
