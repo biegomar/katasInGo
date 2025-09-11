755e6c0e56c65c32dd3253ffee2d9259570ee54e
biegomar <marc.biegota@web.de>
2025-09-11 16:55:56 +0200

    feature: add gildedrose and diamond

---
+++ diamond/diamond.go
@@ -0,0 +1,41 @@
+﻿package diamond
+
+import "strings"
+
+func RenderDiamond(item string) string {
+	if item == "A" {
+		return "A"
+	}
+
+	result := strings.Builder{}
+	baseRune := rune(item[0])
+	distance := baseRune - 'A'
+	gapDistance := 1
+
+	for nextItem := 'A'; nextItem <= baseRune; nextItem++ {
+		startDistance := distance - (nextItem - 'A')
+		result.WriteString(strings.Repeat(" ", int(startDistance)))
+		result.WriteRune(nextItem)
+		if nextItem != 'A' {
+			result.WriteString(strings.Repeat(" ", gapDistance))
+			gapDistance += 2
+			result.WriteRune(nextItem)
+		}
+		result.WriteString("\n")
+	}
+
+	gapDistance -= 4
+	for nextItem := baseRune - 1; nextItem >= 'A'; nextItem-- {
+		startDistance := distance - (nextItem - 'A')
+		result.WriteString(strings.Repeat(" ", int(startDistance)))
+		result.WriteRune(nextItem)
+		if nextItem != 'A' {
+			result.WriteString(strings.Repeat(" ", gapDistance))
+			gapDistance -= 2
+			result.WriteRune(nextItem)
+		}
+		result.WriteString("\n")
+	}
+
+	return result.String()
+}
---
+++ diamond/diamond_test.go
@@ -0,0 +1,18 @@
+﻿package diamond
+
+import "fmt"
+
+func ExampleRenderDiamond() {
+	diamond := RenderDiamond("E")
+	fmt.Println(diamond)
+	// Output:
+	//     A
+	//    B B
+	//   C   C
+	//  D     D
+	// E       E
+	//  D     D
+	//   C   C
+	//    B B
+	//     A
+}
