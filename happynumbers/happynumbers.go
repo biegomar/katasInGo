55bb1ea4eaa61614e4a32a6a5f06e842849b5e01
biegomar <marc.biegota@web.de>
2025-09-12 11:14:25 +0200

    feature: add happy numbers

---
+++ happynumbers/happynumbers.go
@@ -0,0 +1,35 @@
+﻿package happynumbers
+
+import (
+	"strconv"
+)
+
+func IsHappy(n int) bool {
+	seen := make(map[int]bool)
+	iterationSum := n
+	for !seen[iterationSum] && iterationSum != 1 {
+		seen[iterationSum] = true
+		iterationSum = oneCycle(iterationSum)
+	}
+	return iterationSum == 1
+}
+
+func oneCycle(n int) int {
+	squareSum := 0
+	digits := intToDigits(n)
+	for _, digit := range digits {
+		squareSum += digit * digit
+	}
+
+	return squareSum
+}
+
+func intToDigits(n int) []int {
+	var result []int
+	numberAsString := strconv.Itoa(n)
+	for _, digit := range numberAsString {
+		result = append(result, int(digit-'0'))
+	}
+
+	return result
+}
---
+++ happynumbers/happynumbers_test.go
@@ -0,0 +1,21 @@
+﻿package happynumbers
+
+import "testing"
+
+func TestIsHappy(t *testing.T) {
+	testCases := []struct {
+		input    int
+		expected bool
+	}{
+		{19, true},
+		{2, false},
+		{7, true},
+	}
+
+	for _, tc := range testCases {
+		actual := IsHappy(tc.input)
+		if actual != tc.expected {
+			t.Fatalf("Expected %t, got %t", tc.expected, actual)
+		}
+	}
+}
