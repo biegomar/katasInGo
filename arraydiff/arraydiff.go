82345cd543a49c7c08c9acf2ef1ab63f7a2f94cd
biegomar <marc.biegota@web.de>
2025-09-13 10:14:30 +0200

    feature: add kata arraydiff

---
+++ arraydiff/arraydiff.go
@@ -0,0 +1,18 @@
+﻿package arraydiff
+
+func ArrayDiff(a, b []int) []int {
+	var result []int
+
+	for _, valueOfA := range a {
+		found := false
+		for _, valueOfB := range b {
+			if valueOfA == valueOfB {
+				found = true
+			}
+		}
+		if !found {
+			result = append(result, valueOfA)
+		}
+	}
+	return result
+}
---
+++ arraydiff/arraydiff_test.go
@@ -0,0 +1,26 @@
+﻿package arraydiff
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestArrayDiff(t *testing.T) {
+	testCases := []struct {
+		inputA   []int
+		inputB   []int
+		expected []int
+	}{
+		{[]int{1, 2, 3}, []int{2}, []int{1, 3}},
+		{[]int{1, 2, 3}, []int{1, 2}, []int{3}},
+		{[]int{1, 2}, []int{2}, []int{1}},
+	}
+
+	for _, tc := range testCases {
+		actual := ArrayDiff(tc.inputA, tc.inputB)
+		if !slices.Equal(tc.expected, actual) {
+			t.Fatalf("[%d, %d] Expected %d, got %d", tc.inputA, tc.inputB, tc.expected, actual)
+		}
+
+	}
+}
