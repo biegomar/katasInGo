bba3b09f32ba918612a27d92d02985bf1efe9fea
biegomar <marc.biegota@web.de>
2025-09-13 09:46:39 +0200

    feature: add wordcount

---
+++ wordcountreader/wordcountreader.go
@@ -0,0 +1,30 @@
+﻿package wordcountreader
+
+import (
+	"bufio"
+	"os"
+	"strings"
+)
+
+type FileContentInfo struct {
+	Words                     int
+	Characters                int
+	CharactersExcludingSpaces int
+}
+
+func (fileContentInfo *FileContentInfo) ReadFileContent(fileName string) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	fileScanner := bufio.NewScanner(file)
+	fileScanner.Split(bufio.ScanLines)
+
+	for fileScanner.Scan() {
+		fileContentInfo.Words += len(strings.Fields(fileScanner.Text()))
+		fileContentInfo.Characters += len(fileScanner.Text())
+		fileContentInfo.CharactersExcludingSpaces += len(strings.Replace(fileScanner.Text(), " ", "", -1))
+	}
+}
---
+++ wordcountreader/wordcountreader_test.go
@@ -0,0 +1,20 @@
+﻿package wordcountreader
+
+import "testing"
+
+func TestFileContentInfo_ReadFileContent(t *testing.T) {
+	testcases := []struct {
+		filename string
+		expected FileContentInfo
+	}{
+		{"example.txt", FileContentInfo{Words: 18, Characters: 86, CharactersExcludingSpaces: 70}},
+	}
+
+	for _, tc := range testcases {
+		var fileContentInfo FileContentInfo
+		fileContentInfo.ReadFileContent(tc.filename)
+		if fileContentInfo != tc.expected {
+			t.Errorf("Expected %v, got %v", tc.expected, fileContentInfo)
+		}
+	}
+}
