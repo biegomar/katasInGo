c1e6de85c1d29046aedd6f5b79827189f755c9dc
biegomar <marc.biegota@web.de>
2025-09-19 23:54:07 +0200

    feature: add validate IP

---
+++ ipvalidation/ipvalidation.go
@@ -0,0 +1,31 @@
+﻿package ipvalidation
+
+import (
+	"strconv"
+	"strings"
+)
+
+func Is_valid_ip(ip string) bool {
+	octets := strings.Split(ip, ".")
+
+	if len(octets) != 4 {
+		return false
+	}
+
+	for _, octet := range octets {
+		number, err := strconv.Atoi(octet)
+		if err != nil {
+			return false
+		}
+
+		if number < 0 || number > 255 {
+			return false
+		}
+
+		if len(octet) > 1 && octet[0] == '0' {
+			return false
+		}
+	}
+
+	return true
+}
---
+++ ipvalidation/ipvalidation_test.go
@@ -0,0 +1,25 @@
+﻿package ipvalidation
+
+import "testing"
+
+func TestIpValidation(t *testing.T) {
+	testCases := []struct {
+		input    string
+		expected bool
+	}{
+		{"127.0.0.1", true},
+		{"127.0.0.1.1", false},
+		{"1.2.3.4", true},
+		{"1.2.3", false},
+		{"123.456.78.90", false},
+		{"123.045.067.089", false},
+		{"123.45.67.89", true},
+	}
+
+	for _, tc := range testCases {
+		actual := Is_valid_ip(tc.input)
+		if actual != tc.expected {
+			t.Fatalf("[%s] Expected %t, got %t", tc.input, tc.expected, actual)
+		}
+	}
+}
